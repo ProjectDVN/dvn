d19849a1243d9c834766d6a61ec6bad53b1b13e5
bauss-dev <jacobbauss@gmail.com>
2024-12-16 22:00:14 +0100

    Added a "runDVN" function that you should call from your main function
    This avoids a lot of boilerplate code that you would have to add manually before.

--- source/dvn/package.d
+++ source/dvn/package.d
@@ -7,4 +7,42 @@ public
   import dvn.views;
   import dvn.music;
   import dvn.events;
+
+  void runDVN()
+  {
+    import zid;
+    
+    auto gameSettings = loadGameSettings("data/settings.json");
+    setGlobalSettings(gameSettings);
+      
+    auto app = new Application;
+    app.messageLevel = 15;
+
+    foreach (k,v; gameSettings.fonts)
+    {
+      app.fonts.load(k, v);
+    }
+
+    foreach (backupFont; gameSettings.backupFonts)
+    {
+      app.fonts.addBackupFont(backupFont);
+    }
+
+    if (gameSettings.muteMusic) EXT_DisableMusic();
+    if (gameSettings.muteSoundEffects) EXT_DisableSoundEffects();
+    if (gameSettings.volume > 0)
+    {
+      EXT_SetSoundVolume(gameSettings.volume);
+    }
+
+    auto title = gameSettings.title;
+
+    auto window = app.createWindow(title, IntVector(800, 450), false);
+    window.backgroundColor = getColorByName("black");
+
+    window.addView!LoadingView("Loading");
+	  window.fadeToView("Loading", getColorByName("black"), false);
+
+    app.start(false);
+  }
 }
