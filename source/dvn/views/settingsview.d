feb9b69289e613dd9b4cd9d438e322b3bd377ea7
bauss-dev <jacobbauss@gmail.com>
2025-12-14 18:08:35 +0100

    background video for settings view

--- source/dvn/views/settingsview.d
+++ source/dvn/views/settingsview.d
@@ -109,6 +109,14 @@ public final class SettingsView : View
             (window.height / 2) - (bgImage.height / 2));
         bgImage.show();
 
+        if (settings.mainBackgroundVideo && settings.mainBackgroundVideo.length)
+        {
+            auto video = new Video(window, settings.mainBackgroundVideo);
+            addComponent(video);
+            video.size = IntVector(1280, 720);
+            video.position = IntVector(0, 0);
+        }
+
         auto backLabel = new Label(window);
         addComponent(backLabel);
         backLabel.fontName = settings.defaultFont;
