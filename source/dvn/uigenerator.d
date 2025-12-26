ece137f74c4729876e9233ef889283c6582edd91
bauss-dev <jacobbauss@gmail.com>
2025-12-26 20:15:43 +0100

    Added more components to the html/css engine

--- source/dvn/uigenerator.d
+++ source/dvn/uigenerator.d
@@ -7,6 +7,7 @@ import dvn.ui;
 import std.array : join, split;
 import std.conv : to;
 import std.string : strip;
+import std.file : exists;
 
 void generateHtmlUI(string language, string html, string css, Window window, View view, void delegate(Component,HtmlNode) initializer)
 {
@@ -43,6 +44,7 @@ Component generateHtmlUIComponent(string language, Window window, View view, Htm
     Component component;
     Panel panel;
     ScrollBar scrollbar;
+    DropDown dropdown;
 
     void addComponent(Component c)
     {
@@ -54,9 +56,21 @@ Component generateHtmlUIComponent(string language, Window window, View view, Htm
     switch (node.name)
     {
         case "img":
-            auto image = new Image(window, node.getAttribute("src").value);
-            addComponent(image);
-            component = image;
+            auto imageSource = node.getAttribute("src").value;
+            auto isAnimation = node.hasAttribute("animation");
+
+            if (isAnimation)
+            {
+                auto animation = new Animation(window, imageSource, node.hasAttribute("repeat"));
+                addComponent(animation);
+                component = animation;
+            }
+            else
+            {
+                auto image = new Image(window, imageSource, exists(imageSource));
+                addComponent(image);
+                component = image;
+            }
             break;
 
         case "section":
@@ -125,7 +139,187 @@ Component generateHtmlUIComponent(string language, Window window, View view, Htm
             component = label;
             break;
 
+        case "video":
+            auto video = new Video(window, node.getAttribute("src").value, node.hasAttribute("repeat"));
+            addComponent(video);
+            component = video;
+            break;
+            
+        case "button":
+            auto button = new Button(window);
+            addComponent(button);
+            button.text = parseLocalizedString(language, node.text.strip).to!dstring;
+
+            foreach (attribute; attributes)
+            {
+                switch (attribute.name)
+                {
+                    case "ui-font-family":
+                        button.fontName = attribute.value;
+                        break;
+
+                    case "ui-font-size":
+                        button.fontSize = to!int(attribute.value);
+                        break;
+
+                    case "ui-color":
+                        auto colorValues = attribute.value.split(" ");
+                        button.textColor = colorValues[0].getColorByHex;
+                        if (colorValues.length > 1)
+                        {
+                            button.textColor = button.textColor.changeAlpha(to!int(colorValues[1]));
+                        }
+                        break;
+
+                    default: break;
+                }
+            }
+
+            button.fitToSize = false;
+            component = button;
+            break;
+
+        case "input":
+            switch (node.getAttribute("type").value)
+            {
+                case "text":
+                    auto textbox = new TextBox(window);
+                    addComponent(textbox);
+
+                    foreach (attribute; attributes)
+                    {
+                        switch (attribute.name)
+                        {
+                            case "ui-font-family":
+                                textbox.fontName = attribute.value;
+                                break;
+
+                            case "ui-font-size":
+                                textbox.fontSize = to!int(attribute.value);
+                                break;
+
+                            case "ui-color":
+                                auto colorValues = attribute.value.split(" ");
+                                textbox.textColor = colorValues[0].getColorByHex;
+                                if (colorValues.length > 1)
+                                {
+                                    textbox.textColor = textbox.textColor.changeAlpha(to!int(colorValues[1]));
+                                }
+                                break;
+
+                            default: break;
+                        }
+                    }
+
+                    auto maxCharacters = node.getAttribute("maxlength");
+                    if (maxCharacters && maxCharacters.value && maxCharacters.value.length)
+                    {
+                        textbox.maxCharacters = to!int(maxCharacters.value);
+                    }
+
+                    auto textPadding = node.getAttribute("padding");
+                    if (textPadding && textPadding.value && textPadding.value.length)
+                    {
+                        textbox.textPadding = to!int(textPadding.value);
+                    }
+
+                    auto hideCharacter = node.getAttribute("hidechar");
+                    if (hideCharacter && hideCharacter.value && hideCharacter.value.length)
+                    {
+                        textbox.hideCharacter = hideCharacter.value[0].to!dchar;
+                    }
+
+                    textbox.restyle();
+
+                    component = textbox;
+                    break;
+
+                default: break;
+            }
+            break;
+
+        case "select":
+            dropdown = new DropDown(window);
+            addComponent(dropdown);
+
+            foreach (attribute; attributes)
+            {
+                switch (attribute.name)
+                {
+                    case "ui-font-family":
+                        dropdown.fontName = attribute.value;
+                        break;
+
+                    case "ui-font-size":
+                        dropdown.fontSize = to!int(attribute.value);
+                        break;
+
+                    case "ui-color":
+                        auto colorValues = attribute.value.split(" ");
+                        dropdown.textColor = colorValues[0].getColorByHex;
+                        if (colorValues.length > 1)
+                        {
+                            dropdown.textColor = dropdown.textColor.changeAlpha(to!int(colorValues[1]));
+                        }
+                        break;
+
+                    default: break;
+                }
+            }
+
+            component = dropdown;
+            break;
+
         default: return null;
+    }
+
+    auto width = node.getAttribute("width");
+    auto height = node.getAttribute("height");
+
+    if (width && width.value && width.value.length &&
+        height && height.value && height.value.length)
+    {
+        component.size = IntVector(to!int(width.value), to!int(height.value));
+    }
+
+    if (dropdown)
+    {   
+        dropdown.restyle();
+
+        dstring selected;
+
+        foreach (child; node.children)
+        {
+            if (child.name != "option")
+            {
+                continue;
+            }
+
+            auto valueAttribute = child.getAttribute("value");
+            string value;
+            if (valueAttribute && valueAttribute.value && valueAttribute.value.length)
+            {
+                value = valueAttribute.value;
+            }
+            else
+            {
+                value = child.text;
+            }
+
+            if (value && value.length && child.hasAttribute("selected"))
+            {
+                selected = value.to!dstring;
+            }
+
+            dropdown.addItem(child.text.to!dstring, value);
+        }
+
+        if (selected && selected.length)
+        {
+            dropdown.setItem(selected);
+        }
+
+        dropdown.restyle();
     }
 
     if (component)
@@ -142,30 +336,38 @@ Component generateHtmlUIComponent(string language, Window window, View view, Htm
                 case "ui-position":
                     if (!sectionComponent)
                     {
-                        switch (attribute.value)
+                        auto values = attribute.value.split(" ");
+                        if (values.length == 2)
                         {
-                            case "center":
-                                if (parent)
-                                {
-                                    component.position = IntVector(
-                                        (parent.width / 2) - (component.width / 2),
-                                        (parent.height / 2) - (component.height / 2)
-                                    );
-                                }
-                                else
-                                {
-                                    component.position = IntVector(
-                                        (window.width / 2) - (component.width / 2),
-                                        (window.height / 2) - (component.height / 2)
-                                    );
-                                }
-                                break;
+                            component.position = IntVector(to!int(values[0]), to!int(values[1]));
+                        }
+                        else
+                        {
+                            switch (attribute.value)
+                            {
+                                case "center":
+                                    if (parent)
+                                    {
+                                        component.position = IntVector(
+                                            (parent.width / 2) - (component.width / 2),
+                                            (parent.height / 2) - (component.height / 2)
+                                        );
+                                    }
+                                    else
+                                    {
+                                        component.position = IntVector(
+                                            (window.width / 2) - (component.width / 2),
+                                            (window.height / 2) - (component.height / 2)
+                                        );
+                                    }
+                                    break;
 
-                            case "top-center":
-                                component.anchor = Anchor.top;
-                                break;
+                                case "top-center":
+                                    component.anchor = Anchor.top;
+                                    break;
 
-                            default: break;
+                                default: break;
+                            }
                         }
                     }
                     break;
